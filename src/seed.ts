0cf8433516961c49825432d877a1f816737b1adb
Hasan Ahmad <hasanahmad.dev@gmail.com>
2022-09-11 01:54:40 +0300

    seed functions

--- src/seed.ts
+++ src/seed.ts
@@ -1,6 +1,9 @@
 import { NextFunction, Request, Response } from "express";
 import * as AdminService from "./app/services/admin.service";
+import * as ResumeService from "./app/services/resume.service";
+import * as ServiceService from "./app/services/service.service";
 import { ADMIN_EMAIL, ADMIN_NAME, ADMIN_PASSWORD } from "./config";
+import { resumeData, servicesData } from "./data";
 
 const admin = {
   name: ADMIN_NAME,
@@ -11,6 +14,8 @@ const admin = {
 export async function seed(req: Request, res: Response, next: NextFunction) {
   try {
     await AdminService.createAdmin(admin);
+    await ResumeService.createResume(resumeData);
+    await ServiceService.createServices(servicesData);
     res.status(201).json({
       message: "Created!",
     });
