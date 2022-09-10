33061a33c81ce494bb993ccc8c382b3b7dab10d8
Hasan Ahmad <hasanahmad.dev@gmail.com>
2022-09-11 01:52:15 +0300

    add meta data

---
+++ src/data.ts
@@ -0,0 +1,93 @@
+export const resumeData = {
+  summary:
+    " I'm a Software Engineer with over 4 years of experience. I'm from Syria. I design and create web services for amazing people around the world. I like work with new people. New people are new experiences.",
+  name: "Hasan Ali Ahmad",
+  birthday: "1th Jan 2001",
+  nationality: "Syrian",
+  address: "Dubai",
+  phone: "(971) 50 883 8637",
+  email: "hasanahmadsw@gmail.com",
+  experiences: [
+    {
+      title: "Software Development Engineer",
+      date: "Jan 2022 - Present",
+      company: "MAP Events & Art Production",
+      location: "Dubai",
+      breif:
+        "Design, programming and management of the websites of our company's affiliates, developing a software project plan and managing its deliverables.",
+    },
+    {
+      title: "Founder & Backend Developer",
+      date: "Jan 2021 - Dec 2021",
+      company: "Yallawin",
+      location: "Damascuse - Syria",
+      breif:
+        "Leading and managing the software project team, Ensuring responsiveness of applications, Designing and developing APIs and writing their technical documentation",
+    },
+    {
+      title: "Web Developer",
+      date: "Jan 2019 - Present",
+      company: "Freelance",
+      location: "Remotely",
+      breif:
+        "Developing frontend and backend website architecture and applications, creating servers and databases for functionality, ensuring responsiveness of applications.",
+    },
+  ],
+  educations: [
+    {
+      title: "Bachelor's of Information Technology",
+      date: "2022 - Present",
+      university: "Syrian Virtual University",
+      breif:
+        "Studying the information technology major in the virtual university enables me to study and work at the same time, and this was the main reason for my transfer",
+    },
+    {
+      title: "Bachelor's degree in Software Engineering",
+      date: "2018 - 2021 Paused",
+      university: "Yarmouk Private University",
+      breif:
+        "I completed four years of study in software engineering and programming and stopped and enrolled in a second university",
+    },
+  ],
+  certificates: [
+    {
+      title: "ICPC Syrian University Contestant",
+      date: "2021",
+      source: "ICPC",
+      breif: "The 2021 Syrian Universities Collegiate Programming Contest",
+    },
+    {
+      title: "ICPC Syrian Private University Contestant",
+      date: "2020",
+      source: "ICPC",
+      breif:
+        "The 2020 Syrian Private Universities Collegiate Programming Contest",
+    },
+  ],
+};
+
+export const servicesData = [
+  {
+    slug: "web-development",
+    title: "Web Services Development",
+    breif:
+      "I help you to build effective web services for small and large business",
+  },
+  {
+    slug: "web-design",
+    title: "Web Design",
+    breif:
+      "Design direction for business. Get your business on the next level. I help to create great experiences.",
+  },
+  {
+    slug: "ui-ux",
+    title: "UI UX Solutions",
+    breif: "Design clear and effective user interfaces.",
+  },
+  {
+    slug: "social-media",
+    title: "Socila Media Solution",
+    breif:
+      "Business development by customizing its social media and contributing to its growth.",
+  },
+];
